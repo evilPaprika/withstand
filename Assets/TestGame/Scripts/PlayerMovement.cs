05954031a1cee950285b7a9aaf44a1bdf674a31f
evilPaprika <ivil.paprika@gmail.com>
2018-03-08 21:52:03 +0500

    added assets

---
+++ Assets/TestGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Completed;
+using UnityEngine;
+
+public class PlayerMovement : MovingObject
+{
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Debug.Log("OnCantMove called");
+    }
+
+    // Update is called once per frame
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed,
+            Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed, 0f);
+	}
+}
