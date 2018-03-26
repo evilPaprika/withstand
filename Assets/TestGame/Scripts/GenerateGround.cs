137731e10ead32f96d79cb216af937455b85a94d
evilPaprika <ivil.paprika@gmail.com>
2018-03-26 22:42:43 +0500

    немного пописал землю, и плавная камера

---
+++ Assets/TestGame/Scripts/GenerateGround.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGround : MonoBehaviour
+{
+    public GameObject[] floorTiles;
+
+    private Transform tilesHolder;
+
+    // Use this for initialization
+    void Start()
+    {
+        tilesHolder = new GameObject("FloorTiles").transform;
+
+        for (int x = -50; x < 50; x++)
+        {
+            for (int y = -50; y < 50; y++)
+            {
+                var toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+
+
+                var instance =
+                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity);
+
+
+                instance.transform.SetParent(tilesHolder);
+            }
+        }
+        foreach (var obj in tilesHolder)
+        {
+            print(obj.ToString());
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+    }
+}
